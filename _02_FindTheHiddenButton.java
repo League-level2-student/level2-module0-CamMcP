7ddcb1aea72b9fa3a8e99df832b0c557a58e9af5
CamMcP <52165008+CamMcP@users.noreply.github.com>
2021-03-22 00:58:26 +0000

    www

---
+++ _02_FindTheHiddenButton.java
@@ -0,0 +1,79 @@
+/*****
+ * 
+ * Run the FndHdnBtn.jar to play the finished game
+ * 
+ *****/
+
+package arrays;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+	
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_FindTheHiddenButton implements ActionListener{
+	JFrame frame;
+	JPanel panel;
+	
+	//1. create an array of JButtons. Don't initialize it yet.
+	JButton[] buttons;
+	//2 create an int variable called hiddenButton
+	int hiddenButton;
+	public static void main(String[] args) {
+		new _02_FindTheHiddenButton().start();
+	}
+	
+	public void start() {
+		frame = new JFrame("Find the Button");
+		panel = new JPanel();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//3.  Ask the user to enter a positive number and convert it to an int
+	String answer =	JOptionPane.showInputDialog("Please input a positive number less than 2016");
+	Integer.parseInt(answer);
+		//4. Initialize the array of JButtons to be the size of the int created in step 3
+		buttons = new JButton[2016];
+		//5. Make a for loop to iterate through the JButton array
+		for (int i = 0; i < buttons.length; i++) {
+			buttons[i] = new JButton();
+			buttons[i].addActionListener(this);
+			panel.add(buttons[i]);
+		}
+			//6. initialize each JButton in the array
+			//7. add the ActionListener to each JButton
+			//8. add each JButton to the panel
+		
+		//9 add the panel to the window
+		frame.add(panel);
+		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		//11. set the JFrame to visible.
+		frame.setVisible(true);
+		//12. Give the user the instructions for the game.
+		JOptionPane.showMessageDialog(null, "Hello there user. You have been previously asked to eneter a number. Now that you have answered that, a random button has been selected that is lower than the number you chose. Have fun!");
+		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
+		Random rand = new Random();
+		rand.nextInt(answer.length());
+		JOptionPane.showMessageDialog(null, rand);
+		//14. Set the text of the JButton located at hiddenButton to  "ME"
+
+		//15. Use Thread.sleep(1000); to pause the program.
+		//    Surround it with a try/catch - use Eclipse helper for this
+		
+		//16. Set the text of the JButton located at hiddenButton to be blank.
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton buttonClicked = (JButton)e.getSource();
+		
+		//17. if the hiddenButton is clicked, tell the user that they win.
+		
+		//18. else tell them to try again
+	}
+}
