d3a135162a095ffc6a6ca1c4d068923309f5d217
CamMcP <52165008+CamMcP@users.noreply.github.com>
2021-04-05 01:01:07 +0000

    findButton

--- _02_FindTheHiddenButton.java
+++ _02_FindTheHiddenButton.java
@@ -33,10 +33,10 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-	String answer =	JOptionPane.showInputDialog("Please input a positive number less than 2016");
-	Integer.parseInt(answer);
+	String answer =	JOptionPane.showInputDialog("Please input a positive number lower than 2297");
+	int gg = Integer.parseInt(answer);
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		buttons = new JButton[2016];
+		buttons = new JButton[gg];
 		//5. Make a for loop to iterate through the JButton array
 		for (int i = 0; i < buttons.length; i++) {
 			buttons[i] = new JButton();
@@ -54,11 +54,10 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		//11. set the JFrame to visible.
 		frame.setVisible(true);
 		//12. Give the user the instructions for the game.
-		JOptionPane.showMessageDialog(null, "Hello there user. You have been previously asked to eneter a number. Now that you have answered that, a random button has been selected that is lower than the number you chose. Have fun!");
-		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
+		JOptionPane.showMessageDialog(null, "You have been previously asked to input a number. A random button less than that has been selected, and you must find it! good luck!");
 		Random rand = new Random();
-		rand.nextInt(answer.length());
-		JOptionPane.showMessageDialog(null, rand);
+		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
+		hiddenButton = rand.nextInt(gg);
 		//14. Set the text of the JButton located at hiddenButton to  "ME"
 
 		//15. Use Thread.sleep(1000); to pause the program.
